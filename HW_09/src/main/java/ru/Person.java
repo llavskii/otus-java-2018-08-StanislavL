91872a657cfccbc0bc08ffcfdbf1566925afef89
llavskii <kieaki@gmail.com>
2019-03-31 22:44:23 +0300

    HW_09. add set of numeric and string classes

--- HW_09/src/main/java/ru/Person.java
+++ HW_09/src/main/java/ru/Person.java
@@ -3,7 +3,10 @@ package ru;
 public class Person {
     private String name;
     private String surname;
+    private boolean isMan = true;
+    private char flag = 'g';
     private int age;
+    private Integer year;
     private Person parent;
     private Person child;
 
@@ -11,5 +14,14 @@ public class Person {
         this.name = name;
         this.surname = surname;
         this.age = age;
+    }
+
+    public Person(String name, String surname, int age, Integer year, Person parent, Person child) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.year = year;
+        this.parent = parent;
+        this.child = child;
     }
 }
